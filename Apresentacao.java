ad3f4ed911347c29a6b191bd0ba67410cc52493d
enzo maass <enzo_maass@estudante.sesisenai.org.br>
2025-12-18 20:31:52 -0300

    exercicio2 feito

---
+++ Apresentacao.java
@@ -0,0 +1,20 @@
+// EXERCÍCIO 2: APRESENTAÇÃO PESSOAL
+
+public class Apresentacao {
+
+    public static void main(String[] args) {
+
+        // Declaração das variáveis
+        String nome = "Enzo";
+        int idade = 16;
+        String cidade = "São Paulo";
+
+        // Impressão das informações
+        System.out.println("=== APRESENTAÇÃO ===");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Cidade: " + cidade);
+
+    }
+
+}
